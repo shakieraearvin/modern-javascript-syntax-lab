9d76d1737a8780d69af93f128402ab9a8017429d
Shakiera Earvin <kieraearvin@gmail.com>
2025-03-04 11:33:43 -0500

    exercise 6 done

--- app.js
+++ app.js
@@ -97,3 +97,24 @@ const anotherCar = {
   console.log('Original: ', anotherCar);
   console.log('Clone: ', myCar);
   
+  // Exercise 6: Dynamic keys in objects
+// a. Define a variable named `propertyName` and assign a string (like 
+//    'username', 'age', or 'email') to it.
+// 
+// b. Create an object named `userProfile`. 
+// 
+// c. Use `propertyName` as a dynamic key in `userProfile`. Assign it a 
+//    relevant value.
+//
+// d. Console log the `userProfile` object to see the result.
+//
+// Your code here:
+
+const propertyName = 'age';
+
+const userProfile = {
+    [propertyName]: 35,
+};
+
+console.log(userProfile);
+
